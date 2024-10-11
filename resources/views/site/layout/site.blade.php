692ca38ad7fb2c1721c3664daf61a2610ef2690a
slusarvitaliy <slusarvitaliy@gmail.com>
2024-10-11 17:08:29 +0300

    added meta seo tags

--- resources/views/site/layout/site.blade.php
+++ resources/views/site/layout/site.blade.php
@@ -1,12 +1,22 @@
 <!doctype html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>{{env('APP_NAME')}}</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
+    <meta name="description" content="Fit360 фітнес студія у Полтаві">
+    <meta name="keywords" content="fit360, fitnes, poltava, полтаваб фітнес, спорт, тренування">
+    <meta name="author" content="Олександр Пузій">
+    <meta name="image" content="{{asset('assets/images/header/fit360_logo.jpg')}}">
+
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="Fit360 фітнес студія у Полтаві">
+    <meta property="og:description" content="Fit360 фітнес студія у Полтаві">
+    <meta property="og:image" content="{{asset('assets/images/header/fit360_logo.jpg')}}">
+    <meta property="og:url" content="{{env('APP_URL')}}">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
