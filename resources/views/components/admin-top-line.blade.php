922d97cf1620acd4a6546e3c0e6d0b545cd2c6d4
slusarvitaliy <slusarvitaliy@gmail.com>
2024-10-12 14:36:56 +0300

    fixed component

--- resources/views/components/admin-top-line.blade.php
+++ resources/views/components/admin-top-line.blade.php
@@ -1,5 +1,8 @@
-@if(auth()->user()->isAdmin())
-    <div>
-        <a href="/admin/login">Адмінка</a>
-    </div>
-@endif
+@auth()
+    @if(auth()->user()->isAdmin())
+        <div>
+            <a href="/admin/login">Адмінка</a>
+        </div>
+    @endif
+@endauth
+
