19dcf1df49cfc30954ad364786e31ef5d1fce9fd
slusarvitaliy <slusarvitaliy@gmail.com>
2024-11-06 18:54:24 +0200

    added title

--- resources/views/site/layout/site.blade.php
+++ resources/views/site/layout/site.blade.php
@@ -6,6 +6,7 @@
     <title>{{env('APP_NAME')}}</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <title>Fit360 фітнец студія - Полава</title>
 
     <meta name="description" content="Fit360 фітнес студія у Полтаві">
     <meta name="keywords" content="fit360, fitnes, poltava, полтаваб фітнес, спорт, тренування">
