6682c73ce9628789ba8e2ca19a006497f71523f2
slusarvitaliy <slusarvitaliy@gmail.com>
2025-07-12 17:58:05 +0300

    feat: improve survey display and enhance user interaction in the response form

--- resources/views/themes/simple-gray/surveys/show.blade.php
+++ resources/views/themes/simple-gray/surveys/show.blade.php
@@ -4,105 +4,97 @@
 ])
 
 @section('content')
-    <div class="container mx-auto py-8 px-4">
-        <div class="max-w-3xl mx-auto p-8">
-            <h1 class="text-3xl font-bold text-gray-800 mb-3">{{ $survey->title }}</h1>
+    <div class="container mx-auto py-4 px-0">
+        <div class="max-w-full mx-auto bg-white shadow-xl rounded-2xl p-4">
+            <h1 class="text-4xl font-extrabold text-gray-900 mb-4 text-center">{{ $survey->title }}</h1>
+
             @if($survey->description)
-                <p class="text-gray-600 mb-8 text-ellipsis p-2">{{ $survey->description }}</p>
+                <p class="text-gray-600 mb-8 leading-relaxed">{{ $survey->description }}</p>
             @endif
 
-            <form action="{{ route('surveys.submit', $survey->id) }}" method="POST" class="space-y-6">
+            <form action="{{ route('surveys.submit', $survey->id) }}" method="POST" class="space-y-4 flex flex-col">
                 @csrf
 
                 @foreach($survey->questions as $index => $question)
-                    <div class="bg-white p-6 rounded-lg shadow-sm border border-gray-200 my-2">
-                        <h3 class="text-xl font-semibold text-gray-800 mb-4">{{ $question['question'] }}</h3>
+                    <div class="p-4 bg-gray-50 rounded-xl border border-gray-200 shadow-sm">
+                        <h3 class="font-medium text-gray-800 mb-4">{{ $question['question'] }}</h3>
 
-                        <div class="mt-4">
+                        <div>
                             @switch($question['type'])
                                 @case(App\Enums\SurveyTypeEnum::YES_NO->value)
-                                    <div class="space-y-2 flex flex-wrap gap-4">
-                                        <div class="flex items-center">
-                                            <input id="yes-{{ $index }}" name="answers[{{ $index }}]" type="radio" value="yes"
-                                                class="h-5 w-5 text-blue-600 border-gray-300 focus:ring-blue-500">
-                                            <label for="yes-{{ $index }}" class="ml-3 text-gray-700 p-2">Так</label>
-                                        </div>
-                                        <div class="flex items-center">
-                                            <input id="no-{{ $index }}" name="answers[{{ $index }}]" type="radio" value="no"
-                                                class="h-5 w-5 text-blue-600 border-gray-300 focus:ring-blue-500">
-                                            <label for="no-{{ $index }}" class="ml-3 text-gray-700 p-2">Ні</label>
-                                        </div>
+                                    <div class="flex gap-6">
+                                        <label class="flex items-center space-x-2 cursor-pointer">
+                                            <input id="yes-{{ $index }}" name="answers[{{ $index }}]" type="radio" value="yes" class="form-radio text-blue-600">
+                                            <span class="px-2">Так</span>
+                                        </label>
+                                        <label class="flex items-center space-x-2 cursor-pointer">
+                                            <input id="no-{{ $index }}" name="answers[{{ $index }}]" type="radio" value="no" class="form-radio text-red-600">
+                                            <span class="px-2">Ні</span>
+                                        </label>
                                     </div>
                                     @break
 
                                 @case(App\Enums\SurveyTypeEnum::PLUS_MINUS->value)
-                                    <div class="space-y-2 flex flex-wrap gap-4">
-                                        <div class="flex items-center">
-                                            <input id="plus-{{ $index }}" name="answers[{{ $index }}]" type="radio" value="plus"
-                                                class="h-5 w-5 text-blue-600 border-gray-300 focus:ring-blue-500">
-                                            <label for="plus-{{ $index }}" class="ml-3 text-gray-700 p-2">+</label>
-                                        </div>
-                                        <div class="flex items-center">
-                                            <input id="minus-{{ $index }}" name="answers[{{ $index }}]" type="radio" value="minus"
-                                                class="h-5 w-5 text-blue-600 border-gray-300 focus:ring-blue-500">
-                                            <label for="minus-{{ $index }}" class="ml-3 text-gray-700 p-2">-</label>
-                                        </div>
+                                    <div class="flex gap-6">
+                                        <label class="flex items-center space-x-2 cursor-pointer">
+                                            <input id="plus-{{ $index }}" name="answers[{{ $index }}]" type="radio" value="plus" class="form-radio text-green-500">
+                                            <span class="px-2">+</span>
+                                        </label>
+                                        <label class="flex items-center space-x-2 cursor-pointer">
+                                            <input id="minus-{{ $index }}" name="answers[{{ $index }}]" type="radio" value="minus" class="form-radio text-red-500">
+                                            <span class="px-2">-</span>
+                                        </label>
                                     </div>
                                     @break
 
                                 @case(App\Enums\SurveyTypeEnum::TEXT->value)
-                                    <div>
-                                        <textarea name="answers[{{ $index }}]" rows="3"
-                                            class="block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 text-gray-800 p-2"
-                                            placeholder="Ваша відповідь..."></textarea>
-                                    </div>
+                                    <textarea name="answers[{{ $index }}]" rows="3"
+                                              class="w-full rounded-lg border border-gray-300 p-3 focus:ring-2 focus:ring-blue-500 focus:outline-none"
+                                              placeholder="Ваша відповідь..."></textarea>
                                     @break
 
                                 @case(App\Enums\SurveyTypeEnum::RATING->value)
-                                    <div class="flex space-x-4 gap-2">
+                                    <div class="flex flex-wrap gap-4">
                                         @for ($i = 1; $i <= 10; $i++)
-                                            <div class="flex flex-col items-center">
-                                                <input id="rating-{{ $index }}-{{ $i }}" name="answers[{{ $index }}]"
-                                                    type="radio" value="{{ $i }}"
-                                                    class="h-5 w-5 text-blue-600 border-gray-300 focus:ring-blue-500">
-                                                <label for="rating-{{ $index }}-{{ $i }}" class="mt-1 text-gray-700">{{ $i }}</label>
-                                            </div>
+                                            <label class="flex flex-col items-center space-y-1 cursor-pointer">
+                                                <input type="radio" name="answers[{{ $index }}]" value="{{ $i }}"
+                                                       class="form-radio text-yellow-500">
+                                                <span class="text-sm text-gray-600">{{ $i }}</span>
+                                            </label>
                                         @endfor
                                     </div>
                                     @break
 
                                 @case(App\Enums\SurveyTypeEnum::CUSTOM->value)
-                                    <div class="space-y-2 gap-2">
-                                        @if(isset($question['options']))
+                                    @if(isset($question['options']))
+                                        <div class="space-y-3">
                                             @foreach($question['options'] as $optionIndex => $option)
-                                                <div class="flex items-center">
-                                                    <input id="option-{{ $index }}-{{ $optionIndex }}"
-                                                        name="answers[{{ $index }}][]"
-                                                        type="checkbox"
-                                                        value="{{ $option['option'] }}"
-                                                        class="h-5 w-5 text-blue-600 border-gray-300 focus:ring-blue-500">
-                                                    <label for="option-{{ $index }}-{{ $optionIndex }}"
-                                                        class="ml-3 text-gray-700 px-2">{{ $option['option'] }}</label>
-                                                </div>
+                                                <label class="flex items-center space-x-2 cursor-pointer">
+                                                    <input type="checkbox"
+                                                           name="answers[{{ $index }}][]"
+                                                           value="{{ $option['option'] }}"
+                                                           class="form-checkbox text-indigo-600">
+                                                    <span>{{ $option['option'] }}</span>
+                                                </label>
                                             @endforeach
-                                        @endif
-                                    </div>
+                                        </div>
+                                    @endif
                                     @break
 
                                 @default
-                                    <p class="text-gray-500 italic">Тип питання не підтримується</p>
+                                    <p class="text-sm text-gray-500 italic">Тип питання не підтримується</p>
                             @endswitch
                         </div>
                     </div>
                 @endforeach
 
-                <div class="mt-8">
-                    <button type="submit"
-                        class="w-full py-3 px-4 text-white rounded-lg bg-gray-800 ">
+                <div>
+                    <button type="submit" class="w-full py-3 px-6 text-white font-semibold rounded-lg bg-gray-600 hover:bg-gray-700 focus:ring-4 focus:ring-gray-400 transition-all">
                         Надіслати відповіді
                     </button>
                 </div>
             </form>
         </div>
     </div>
+
 @endsection
